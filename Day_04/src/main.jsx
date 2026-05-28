10764f1faf868975355a4034b916b515c1b509ee
kandhwayanushka-hue <kandhwayanushka@gmail.com>
2026-05-28 16:47:34 +0530

    exp

--- Day_04/src/main.jsx
+++ Day_04/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App></App>
 )
+//react element 
+// const reactElement = <div>Hello</div>;
+
+// //react Function  component -> js function which returns JSX;
+// const ReactComponent = (props) => {
+//   return (
+//     <div>Hii {props.name}</div>
+//   )
+// }
